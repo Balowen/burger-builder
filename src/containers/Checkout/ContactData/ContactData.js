195ab3acbe12a67da7c279d4fe1c2dc67d5f5b4c
Bartek <bartlomiejnawoj@gmail.com>
2020-11-03 23:00:17 +0100

    Setting Up a js config for the form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,58 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: '',
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your name',
+				},
+				value: '',
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street',
+				},
+				value: '',
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code',
+				},
+				value: '',
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country',
+				},
+				value: '',
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your E-Mail',
+				},
+				value: '',
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'fastest', displayValue: 'Fastest' }
+						{ value: 'cheapest', displayValue: 'Cheapest' }
+
+					],
+				},
+				value: '',
+			},
 		},
 		loading: false,
 	}
@@ -24,16 +71,6 @@ class ContactData extends Component {
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price,
-			customer: {
-				name: 'Bart Nawoj',
-				address: {
-					street: 'testtest 1',
-					zipCode: '12341',
-					country: 'Poland',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
 		}
 		axios
 			.post('/orders.json', order)
@@ -50,10 +87,7 @@ class ContactData extends Component {
 		let form = (
 			<form>
 				<Input
-					inputtype="input"
-					type="text"
-					name="name"
-					placeholder="Your name"
+					elementType="..." elementConfig="..." value="..."
 				/>
 				<Input
 					inputtype="input"
