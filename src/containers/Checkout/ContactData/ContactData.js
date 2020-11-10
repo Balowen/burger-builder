20195921d79bcdbad6485a17112cabc479df59e2
Bartek <bartlomiejnawoj@gmail.com>
2020-11-10 11:08:18 +0100

    adjusting checkout and Contact Data to use redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import classes from './ContactData.module.css'
 import Spinner from '../../../components/UI/Spinner/Spinner'
@@ -95,7 +96,6 @@ class ContactData extends Component {
 
 	orderHandler = (event) => {
 		event.preventDefault()
-
 		this.setState({ loading: true })
 		const formData = {}
 		for (let formElementIdentifier in this.state.orderForm) {
@@ -104,7 +104,7 @@ class ContactData extends Component {
 			].value
 		}
 		const order = {
-			ingredients: this.props.ingredients,
+			ingredients: this.props.ings,
 			price: this.props.price,
 			orderData: formData,
 		}
@@ -199,4 +199,11 @@ class ContactData extends Component {
 	}
 }
 
-export default ContactData
+const mapStateToProps = (state) => {
+	return {
+		ings: state.ingredients,
+		price: state.totalPrice,
+	}
+}
+
+export default connect(mapStateToProps)(ContactData)
