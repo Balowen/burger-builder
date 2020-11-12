fca58f32c6baaacd314e89023394069ce4d27455
Bartek <bartlomiejnawoj@gmail.com>
2020-11-12 12:58:33 +0100

    refactor Order Actions

--- src/Store/reducers/order.js
+++ src/Store/reducers/order.js
@@ -7,6 +7,11 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
+		case actionTypes.PURCHASE_BURGER_START:
+			return {
+				...state,
+				loading: true,
+			}
 		case actionTypes.PURCHASE_BURGER_SUCCESS:
 			const newOrder = {
 				...action.orderData,
@@ -22,6 +27,7 @@ const reducer = (state = initialState, action) => {
 				...state,
 				loading: false,
 			}
+
 		default:
 			return state
 	}
